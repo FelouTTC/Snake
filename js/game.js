d9b5daf93ce5c7f74b32248db346e702345ae094
Ruslan <55595537+FelouTTC@users.noreply.github.com>
2021-03-06 20:23:45 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,103 +1,130 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const ground = new Image();
-ground.src = "img/ground.png";
-
 const foodImg = new Image();
 foodImg.src = "img/food.png";
 
-let box = 32;
+class SnakeS {
+  constructor() {
+    this.box = 20;
+    this.score = 0;
+    this.snake = [];
+    this.snake[0] = {
+      x: 20 * this.box,
+      y: 20 * this.box
+    };
+    this.food = {
+      x: Math.floor((Math.random() * 40)) * this.box,
+      y: Math.floor((Math.random() * 40 + 5)) * this.box,
+    };
+  }
 
-let score = 0;
 
-let food = {
-  x: Math.floor((Math.random() * 17 + 1)) * box,
-  y: Math.floor((Math.random() * 15 + 3)) * box,
-};
+  createField() {
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, 100);
+    ctx.closePath();
 
-let snake = [];
-snake[0] = {
-  x: 9 * box,
-  y: 10 * box
-};
+
+    for (let i = 0; i <= 40; i++) {
+      for (let j = 5; j <= 45; j++) {
+        ctx.fillStyle = ((i + j) % 2) ? '#34c924' : '#c2ff99';
+        ctx.beginPath();
+        ctx.fillRect(i * this.box, j * this.box, this.box, this.box)
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+Snakee = new SnakeS();
 
 document.addEventListener("keydown", direction);
 
 let dir;
 
 function direction(event) {
-  if(event.keyCode == 37 && dir != "right")
+  if (event.keyCode == 37 && dir != "right")
     dir = "left";
-  else if(event.keyCode == 38 && dir != "down")
+  else if (event.keyCode == 38 && dir != "down")
     dir = "up";
-  else if(event.keyCode == 39 && dir != "left")
+  else if (event.keyCode == 39 && dir != "left")
     dir = "right";
-  else if(event.keyCode == 40 && dir != "up")
+  else if (event.keyCode == 40 && dir != "up")
     dir = "down";
+  else if (event.keyCode == 32)
+    dir = "start";
 }
 
 function eatTail(head, arr) {
-  for(let i = 0; i < arr.length; i++) {
-    if(head.x == arr[i].x && head.y == arr[i].y)
+  for (let i = 0; i < arr.length; i++) {
+    if (head.x == arr[i].x && head.y == arr[i].y) {
       clearInterval(game);
-      if(dir == "start") {
-        location.reload();
-      }
+      reload = setInterval(() => {
+        if (dir == "start") {
+          window.location.reload();
+          clearInterval(reload);
+        }
+      }, 10);
+    }
   }
 }
 
 function drawGame() {
-  ctx.drawImage(ground, 0, 0);
+  Snakee.createField();
+  ctx.drawImage(foodImg, Snakee.food.x, Snakee.food.y, Snakee.box, Snakee.box);
+  ctx.drawImage(foodImg, Snakee.box * 1, Snakee.box * 2);
 
-  ctx.drawImage(foodImg, food.x, food.y);
-
-  for(let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = i == 0 ? "green" : "red";
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+  for (let i = 0; i < Snakee.snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "blue" : "red";
+    ctx.fillRect(Snakee.snake[i].x, Snakee.snake[i].y, Snakee.box, Snakee.box);
+    ctx.stroke();
   }
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = "Black";
   ctx.font = "50px Arial";
-  ctx.fillText(score, box * 2.5, box * 1.7);
+  ctx.fillText(Snakee.score, Snakee.box * 3, Snakee.box * 3.8);
+  let snakeX = Snakee.snake[0].x;
+  let snakeY = Snakee.snake[0].y;
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
-
-  if(snakeX == food.x && snakeY == food.y) {
-    score++;
-    food = {
-      x: Math.floor((Math.random() * 17 + 1)) * box,
-      y: Math.floor((Math.random() * 15 + 3)) * box,
+  if (snakeX == Snakee.food.x && snakeY == Snakee.food.y) {
+    Snakee.score++;
+    Snakee.food = {
+      x: Math.floor((Math.random() * 40)) * Snakee.box,
+      y: Math.floor((Math.random() * 40 + 5)) * Snakee.box,
     };
   } else {
-    snake.pop();
+    Snakee.snake.pop();
   }
 
-  if(snakeX < box || snakeX > box * 17
-    || snakeY < 3 * box || snakeY > box * 17)
-    clearInterval(game);
 
-  if(dir == "left") snakeX -= box;
-  if(dir == "right") snakeX += box;
-  if(dir == "up") snakeY -= box;
-  if(dir == "down") snakeY += box;
+
+  if (dir == "start") window.location.reload();
+  if (dir == "left") snakeX -= Snakee.box;
+  if (dir == "right") snakeX += Snakee.box;
+  if (dir == "up") snakeY -= Snakee.box;
+  if (dir == "down") snakeY += Snakee.box;
+
+  if (snakeX + 1 < 0) {
+    snakeX = Snakee.box * 40
+  } else if (snakeX > Snakee.box * 40) {
+    snakeX = 0
+  } else if (snakeY < 5 * Snakee.box) {
+    snakeY = Snakee.box * 45
+  } else if (snakeY > Snakee.box * 45) {
+    snakeY = Snakee.box * 5;
+  }
 
   let newHead = {
     x: snakeX,
     y: snakeY
   };
 
-  eatTail(newHead, snake);
+  eatTail(newHead, Snakee.snake);
 
-  snake.unshift(newHead);
+  Snakee.snake.unshift(newHead);
 }
 
-let game = setInterval(drawGame, 60);
-
-
-
-
-
-
-//
+let game = setInterval(drawGame, 20);
